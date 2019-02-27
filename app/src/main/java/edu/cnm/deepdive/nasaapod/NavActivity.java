a4f82c5a9c3e006de03dedca60afcd5c4ee850c7
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-02-27 13:56:34 -0700

    Added new NavActivity and image, and home buttons in the toolbar

---
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/NavActivity.java
@@ -0,0 +1,39 @@
+package edu.cnm.deepdive.nasaapod;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class NavActivity extends AppCompatActivity
+    implements OnNavigationItemSelectedListener {
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_nav);
+    BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+    navigation.setOnNavigationItemSelectedListener(this);
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+    boolean handled = true;
+    switch (menuItem.getItemId()){
+      case R.id.navigation_image:
+        //TODO Load image display fragment.
+        break;
+      case R.id.navigation_history:
+        //TODO Load history list fragment.
+        break;
+        default:
+          handled = false;
+    }
+    return handled;
+  }
+
+}
